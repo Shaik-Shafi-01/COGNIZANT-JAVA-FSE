9e8a112699e133fdf3236580a92a0fff1b4ea965
Shafi <shafi36925801@gmail.com>
2026-07-17 12:35:02 +0530

    sonarqube

---
+++ week4/sonarqube-demo/src/main/java/com/cognizant/sonarqube/Main.java
@@ -0,0 +1,19 @@
+package com.cognizant.sonarqube;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to SonarQube Demo");
+
+        int a = 10;
+        int b = 20;
+
+        if (a < b) {
+            System.out.println("A is smaller");
+        } else {
+            System.out.println("B is smaller");
+        }
+
+    }
+}
